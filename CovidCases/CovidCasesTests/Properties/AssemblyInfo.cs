5de0d81797c1f08de1c6716821505a7eaef48124
skroca <sroca261@gmail.com>
2021-08-18 14:25:48 -0600

    MVC Application
    Covid Cases

---
+++ CovidCases/CovidCasesTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CovidCasesTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CovidCasesTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ded7db3d-2c99-4bf3-8b95-bcaf0a8b6896")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CovidCases/CovidCasesTests/TestCases.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CovidCases.Controllers;
+using System.Web.Mvc;
+using CovidCases.Enums;
+using CovidCases.Models;
+
+namespace CovidCasesTests
+{
+    [TestClass]
+    public class TestCases
+    {
+        [TestMethod]
+        public async Task IndexRegionTest()
+        {
+            ReportController report = new ReportController();
+            var result = await report.Index() as ViewResult;
+            Assert.IsNotNull(result.Model);
+        }
+        [TestMethod]
+        public async Task IndexProvincesTest()
+        {
+            ReportViewModel data = new ReportViewModel();
+            data.SelectedRegion = "US";
+            ReportController report = new ReportController();
+            var result = await report.Index(data) as ViewResult;
+            Assert.IsNotNull(result.Model);
+        }
+        [TestMethod]
+        public async Task GenerateCsvFile()
+        {
+            ReportViewModel data = new ReportViewModel();
+            data.SelectedRegion = "US";
+            ReportController report = new ReportController();
+            await report.Index(data);
+            var result = report.GenerateReport(FileType.CSV) as FileContentResult;
+
+            Assert.IsNotNull(result.ContentType);
+            Assert.AreEqual("text/csv", result.ContentType);
+        }
+        [TestMethod]
+        public async Task GenerateXmlFile()
+        {
+            ReportViewModel data = new ReportViewModel();
+            data.SelectedRegion = "US";
+            ReportController report = new ReportController();
+            await report.Index(data);
+            var result = report.GenerateReport(FileType.Xml) as FileContentResult;
+
+            Assert.IsNotNull(result.ContentType);
+            Assert.AreEqual("text/xml", result.ContentType);
+        }
+        [TestMethod]
+        public async Task GenerateJsonFile()
+        {
+            ReportViewModel data = new ReportViewModel();
+            data.SelectedRegion = "US";
+            ReportController report = new ReportController();
+            await report.Index(data);
+            var result = report.GenerateReport(FileType.Json) as FileContentResult;
+            Assert.IsNotNull(result.ContentType);
+            Assert.AreEqual("text/json", result.ContentType);
+        }
+    }
+}
